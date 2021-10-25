b34d413601dbe92f821751c02e5ae6208e7497d6
Joaopcamposs <joaopcamposs@outlook.com>
2021-10-25 17:03:05 -0300

    Requisicoes da api, inicio do layout e mostrar banners dos filmes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import './App.css'
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  
+  useEffect(() =>{
+    const loadAll = async () => {
+      //pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
+  
   return(
-    <div>
-      Hello world!
+    <div className="page">
+      <section className='lists'>
+        {movieList.map((item, key)=>(
+              <MovieRow key={key} title={item.title} items={item.items}  />
+          ))}
+      </section>
     </div>
   );
 }
---
+++ src/Tmdb.js
@@ -0,0 +1,67 @@
+const API_KEY = '079e5bf3ea207b32d86f31b6356c2ac0';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+/*
+ - originais
+ - recomendados (trending)
+ - em alta (top rated)
+ - acao
+ - comedia
+ - terror
+ - romance
+ - documentarios
+*/
+
+const basicFetch = async (endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
+//aula parada em 1:06h
+export default{
+    getHomeList: async () => {
+        return[
+            {
+                slug: 'originals',
+                title: 'Originais do Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'trending',
+                title: 'Recomendados para você',
+                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'toprated',
+                title: 'Em alta',
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'action',
+                title: 'Ação',
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'comedy',
+                title: 'Comédia',
+                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'horror',
+                title: 'Terror',
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'romance',
+                title: 'Romance',
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentários',
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+            },
+        ];
+    }
+}
